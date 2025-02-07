9b923bf2ffcff8749828f4bc3363ffe04c07db6e
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-07 21:48:53 +0530

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Dashboard } from './pages/Dashboard'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+
 function App() {
-  // const [count, setCount] = useState(0)
-      // return <Dashboard/>
-      return <SignIn/>
+      return <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
 }
 
 export default App
