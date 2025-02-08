0e0cb680826b85148d680764fe7507119fb48e4f
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-08 18:10:07 +0530

    SignIn connected with backend, navigating to dasboard completed

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,35 @@
+import axios from "axios";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
+import { BACKEND_URL } from "../config";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function SignIn() {
+        //@ts-ignore
+        const usernameRef = useRef<HTMLInputElement>();
+        //@ts-ignore
+        const passwordRef = useRef<HTMLInputElement>();
+        const navigate = useNavigate();
+        async function signin(){
+            const username = usernameRef.current?.value;
+            const password = passwordRef.current?.value;
+    
+            const response = await axios.post(BACKEND_URL+"/api/v1/signin",{
+                    username,
+                    password
+            })
+            const jwt = response.data.token;
+            localStorage.setItem("token",jwt);
+            navigate("/dashboard")
+        }
+    
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
         <div className="bg-white rounded-xl border min-w-48 p-4">
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
+            <Input ref={usernameRef} placeholder="Username" />
+            <Input ref={passwordRef} placeholder="Password" />
             <div className="flex justify-center">
-                <Button variant="primary" text="SignIn" size="md"/>
+                <Button variant="primary" text="SignIn" size="md" onClick={signin} />
             </div>
         </div>
     </div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,13 +3,14 @@ import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export function SignUp() {
     //@ts-ignore
     const usernameRef = useRef<HTMLInputElement>();
     //@ts-ignore
     const passwordRef = useRef<HTMLInputElement>();
-
+    const navigate = useNavigate();
     async function signup(){
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
@@ -19,8 +20,8 @@ export function SignUp() {
                 password
         })
         alert("You have signed up")
+        navigate("/signin")
     }
-
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
         <div className="bg-white rounded-xl border min-w-48 p-4">
             <Input ref={usernameRef} placeholder="Username" />
