a0f5ce4db2c9c68eb66ed531a03e99bc0482462d
GURURAJ115 <gururajsnaik115@gmail.com>
2025-02-08 17:59:51 +0530

    SignUp connected with backend

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,6 +1,10 @@
-//@ts-ignore
-export function Input({onChange, placeholder}: {placeholder:string; onChange:()=>void}){
+interface InputProps{
+    placeholder: string;
+    ref?: any;
+}
+
+export function Input({ placeholder,ref}: InputProps){
     return <div>
-        <input placeholder={placeholder} type={"text"} className="px-4 py-2 border rounded m-2" onChange={onChange}/>
+        <input ref={ref} placeholder={placeholder} type={"text"} className="px-4 py-2 border rounded m-2" />
     </div>
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,13 +1,32 @@
+import { useRef } from "react";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function SignUp() {
+    //@ts-ignore
+    const usernameRef = useRef<HTMLInputElement>();
+    //@ts-ignore
+    const passwordRef = useRef<HTMLInputElement>();
+
+    async function signup(){
+        const username = usernameRef.current?.value;
+        const password = passwordRef.current?.value;
+
+        await axios.post(BACKEND_URL+"/api/v1/signup",{
+                username,
+                password
+        })
+        alert("You have signed up")
+    }
+
     return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
         <div className="bg-white rounded-xl border min-w-48 p-4">
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
+            <Input ref={usernameRef} placeholder="Username" />
+            <Input ref={passwordRef} placeholder="Password" />
             <div className="flex justify-center">
-                <Button variant="primary" text="SignUp" size="md"/>
+                <Button onClick={signup} variant="primary" text="SignUp" size="md"/>
             </div>
         </div>
     </div>
